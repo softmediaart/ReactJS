ea7dc86a04cf19aabcffcf98411906996d50759f
softmediaart <75039298+softmediaart@users.noreply.github.com>
2021-08-20 01:01:16 +0300

    Add files via upload

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,1 +1,37 @@
+import {useState} from "react";
+
+export const Form = () => {
+
+    const [author, setAuthor]  = useState([{authorValue:''}]);
+    const [message, setMessage]  = useState([{messageValue:''}]);
+    console.log(author);
+    console.log(message);
+
+
+
+    const handleAuthorChange = (e)=> {
+        setAuthor({authorValue: e.target.value});
+
+    }
+    const handleMessageChange = (e)=> {
+        setMessage({messageValue: e.target.value});
+    }
+
+    const handleSubmit = (e) => {
+        alert('Автор: ' + author.authorValue +'Сообщение: ' + message.messageValue );
+
+    }
+
+    return  (
+        <div>
+            <form  onSubmit={handleSubmit}>
+            <input type='text' value={author.authorValue} onChange={handleAuthorChange}/> {" "}
+            <input type='text' value={message.messageValue} onChange={handleMessageChange}/>{" "}
+            <input type="submit" value="Submit" />
+            </form>
+            </div>
+    );
+};
+
+
 
