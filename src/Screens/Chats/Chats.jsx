d53c08d64c531d1f9aaa8b0ba0a644b0760c19d5
softmediaart <75039298+softmediaart@users.noreply.github.com>
2021-08-30 19:44:09 +0300

    Add files via upload

--- src/Screens/Chats/Chats.jsx
+++ src/Screens/Chats/Chats.jsx
@@ -1,1 +1,19 @@
+import {Redirect,useHistory,useParams} from "react-router-dom";
 
+import React from "react";
+
+
+export const Chats = () => {
+
+    const {chatId} = useParams ();
+
+
+
+    if(!chatId) return <Redirect to='/not-found'/>
+
+    console.log(chatId);
+
+
+
+    return <div>Chat</div>;
+};
